2b3dae6bfa2351b702941113baaa3f6630658b7a
Dishi0520 <dishitasarkar@gmail.com>
2024-01-07 22:01:47 -0500

    HackerRank

--- leapyear.py
+++ leapyear.py
@@ -1,9 +1,10 @@
 def is_leap(year):
+    leap = False
     # Write your logic here
-    if year%4==0 and year%100==0 and year%400 != 0:
-        leap = False
-    else year%4==0 and year%100==0 and year%400 == 0:
+    if year%4 == 0 and year%100 == 0 and year%400 == 0:
+        leap = True 
         leap = True
+    
     return leap
 
 year = int(input())
