3b223f98e0182533e2c85afc26a3a9e7aea12f8f
Dishi0520 <dishitasarkar@gmail.com>
2024-01-11 22:15:04 -0500

    Testing function

--- calculator.py
+++ calculator.py
@@ -8,3 +8,6 @@ print(f"{z:.2f}")
 
 def main():
     x = int
+
+
+# assert function - if false there is a error on screef
---
+++ test_calculator.py
@@ -0,0 +1,15 @@
+from calculator2 import square
+
+def main():
+    test_square()
+
+
+def test_square():
+    for i in [-2,-1,0,1,2]:
+        try:
+            assert square(i) == i * i
+        except AssertionError:
+            print(i,"does not equal", i * i)
+
+if __name__ == '__main__':
+    main()                                                            
