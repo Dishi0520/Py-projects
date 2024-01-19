a4cbd4c731ff0194082deb0ce6502d7e48f938c9
Dishi0520 <dishitasarkar@gmail.com>
2024-01-18 21:53:24 -0500

    student name and house

--- students.py
+++ students.py
@@ -1,4 +1,11 @@
+import csv
+
+students = []
+
 with open("students.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        print(f"{name} is in {house}")
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name": row["name"], "home": row["home"]})
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is from {student['home']}")
