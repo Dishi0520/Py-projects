43f2b4089d33de48a6156563d677f5eb3311a9d2
Dishi0520 <dishitasarkar@gmail.com>
2024-01-19 20:30:08 -0500

    gif frames

--- taytaygif.py
+++ taytaygif.py
@@ -9,5 +9,5 @@ for arg in sys.argv[1:]:
     images.append(image)
 
 images[0].save( "TayTay.gif", save_all=True, 
-    append_images=[images[1]],
+    append_images=[images[1],images[2]],
     duration = 200, loop=0 )
